a4e677dc8b110f6775ec080045d3bd4b62edd230
aaronmbos <aaronmbos@gmail.com>
2021-01-04 22:15:00 -0600

    Add TailwindCSS to the project

---
+++ src/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // configure with path to all pages & components to tree-shake production builds
+  purge: ['./pages/**/*.js', './components/**/*.js'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
