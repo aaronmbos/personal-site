185cff23224ec4455c7b5a9f7dfa9a0f180debc6
aaronmbos <aaronmbos@gmail.com>
2021-01-07 23:21:30 -0600

    Add posts.js for getting post related data

---
+++ src/lib/posts.js
@@ -0,0 +1,28 @@
+export function getRecentPosts() {
+  return [
+    {
+      id: "hello-world",
+      title: "Proverbial Hello World!",
+      date: "2020-01-07",
+      description: "This is a test description for the post."
+    },
+    {
+      id: "hello-world",
+      title: "Proverbial Hello World!",
+      date: "2020-01-07",
+      description: "This is a test description for the post."
+    },
+    {
+      id: "hello-world",
+      title: "Proverbial Hello World!",
+      date: "2020-01-07",
+      description: "This is a test description for the post."
+    },
+    {
+      id: "hello-world",
+      title: "Proverbial Hello World!",
+      date: "2020-01-07",
+      description: "This is a test description for the post."
+    }
+  ]
+}
