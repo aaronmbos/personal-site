1ba7c3d6eb0f5a19bd2329f1691da93862809ca7
aaronmbos <aaronmbos@gmail.com>
2021-01-20 23:13:23 -0600

    Fix article width

--- src/components/post-body.js
+++ src/components/post-body.js
@@ -1,6 +1,6 @@
 export default function PostBody({ content }) {
   return (
-    <article className="prose mt-6" dangerouslySetInnerHTML={{ __html: content }}>
+    <article className="prose max-w-none mt-6" dangerouslySetInnerHTML={{ __html: content }}>
     </article>
   );
 }
