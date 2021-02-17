b20c65c4251ee95441868e91f0083f0a61e624e2
aaronmbos <aaronmbos@gmail.com>
2021-02-16 23:13:10 -0600

    Use state for mobile icon open/close and show/hide

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,26 +1,20 @@
 import Link from "next/link";
-
-const toggleMobileMenu = () => {
-  document.getElementById("mobileMenu").classList.toggle("hidden");
-  document.getElementById("mobileMenu").classList.toggle("block");
-  document.getElementById("iconClosed").classList.toggle("hidden");
-  document.getElementById("iconClosed").classList.toggle("block");
-  document.getElementById("iconOpen").classList.toggle("block");
-  document.getElementById("iconOpen").classList.toggle("hidden");
-}
+import { useState } from "react";
 
 export default function Nav({ navLinks }) {
+  const [menuState, setMenuState] = useState(false);
+  
   return (
     <nav className="border-b border-b-gray-200">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <button onClick={toggleMobileMenu} className="inline-flex items-center justify-center p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-expanded="false">
+            <button onClick={() => setMenuState(!menuState)} className="inline-flex items-center justify-center p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-expanded="false">
               <span className="sr-only">Open main menu</span>
-              <svg id="iconClosed" className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="black" viewBox="0 0 24 24" stroke="black" aria-hidden="true">
+              <svg id="iconClosed" className={`${menuState ? 'hidden' : 'block'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="black" viewBox="0 0 24 24" stroke="black" aria-hidden="true">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
               </svg>
-              <svg id="iconOpen" className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="black" viewBox="0 0 24 24" stroke="black" aria-hidden="true">
+              <svg id="iconOpen" className={`${menuState ? 'block' : 'hidden'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="black" viewBox="0 0 24 24" stroke="black" aria-hidden="true">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
               </svg>
             </button>
@@ -49,7 +43,7 @@ export default function Nav({ navLinks }) {
           </div>
         </div>
       </div>
-      <div id="mobileMenu" className="hidden sm:hidden">
+      <div id="mobileMenu" className={`${menuState ? 'block' : 'hidden'} sm:hidden`}>
         <div className="px-2 pt-2 pb-3 space-y-1">
           {navLinks.map((link) => {
             return (
