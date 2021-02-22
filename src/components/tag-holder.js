96b9cb36715e052e102d408bba72ba56c7501b62
aaronmbos <aaronmbos@gmail.com>
2021-02-21 21:05:15 -0600

    Update layout of post tags

--- src/components/tag-holder.js
+++ src/components/tag-holder.js
@@ -2,8 +2,7 @@ import Tag from "./tag";
 
 export default function TagHolder({ tags }) {
   return (
-    <div className="flex">
-      Tags:&nbsp;
+    <div className="flex flex-wrap">
       {tags.map((tag) => {
         return <Tag key={tag.id} tag={tag.tag} />
       })}
