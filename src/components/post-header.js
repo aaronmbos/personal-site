1939e9f26821a415d6bf00ed296bbe5de1fe80c0
aaronmbos <aaronmbos@gmail.com>
2021-11-29 20:47:22 -0600

    Update post h1 size

--- src/components/post-header.js
+++ src/components/post-header.js
@@ -1,7 +1,7 @@
 export default function PostHeader({ title, description, date }) {
   return (
     <>
-      <h1 className="mb-2 font-semibold text-2xl">{title}</h1>
+      <h1 className="mb-2 font-semibold text-3xl">{title}</h1>
       <h2 className="mb-2 text-lg italic">{description}</h2>
       <p className="mb-2 text-gray-600">Published: {date}</p>
       <hr />
