4e16124fe672947fcb3b349cd609e71b19ca410d
aaronmbos <aaronmbos@gmail.com>
2021-12-18 07:26:51 -0600

    Update to latest NextJS and React

--- src/next.config.js
+++ src/next.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  reactStrictMode: true // https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode
-}
+  reactStrictMode: true, // https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode
+  swcMinify: true,
+};
