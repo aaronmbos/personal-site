b778e75cd4c018452037c6204cd90e791301cf40
aaronmbos <aaronmbos@gmail.com>
2022-01-14 00:19:39 -0600

    Setup class based dark theme and begin toggle

--- src/components/nav-body.js
+++ src/components/nav-body.js
@@ -9,6 +9,20 @@ export default function NavBody({ navLinks }) {
             <NavLink key={link.route} route={link.route} text={link.text} />
           );
         })}
+        <div className="flex">
+          <select
+            onChange={(e) =>
+              e.target.value == "Dark"
+                ? document.documentElement.classList.add("dark")
+                : document.documentElement.classList.remove("dark")
+            }
+            className="ml-auto align-self-end dark:text-blue-300 dark:bg-stone-900 bg-blue-50 rounded-md text-blue-500 px-2"
+          >
+            <option>Light</option>
+            <option>Dark</option>
+            <option>System</option>
+          </select>
+        </div>
       </div>
     </div>
   );
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -10,4 +10,5 @@ module.exports = {
     },
   },
   plugins: [typography],
+  darkMode: "class",
 };
