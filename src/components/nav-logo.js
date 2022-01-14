7336409484d8c018ce613ef520fbc7a3c96c80aa
aaronmbos <aaronmbos@gmail.com>
2022-01-13 23:03:35 -0600

    Dark theme updates for the Nav components

--- src/components/nav-logo.js
+++ src/components/nav-logo.js
@@ -4,7 +4,7 @@ export default function NavLogo() {
   return (
     <div className="shrink-0 flex items-center">
       <Link href="/">
-        <a className="hover:text-blue-700 text-blue-500 cursor-pointer text-2xl font-mono font-bold flex items-center flex-none px-2">
+        <a className="dark:text-blue-300 dark:hover:text-blue-100 hover:text-blue-700 text-blue-500 cursor-pointer text-2xl font-mono font-bold flex items-center flex-none px-2">
           &lt;Aaron Bos&#47;&gt;
         </a>
       </Link>
--- src/components/nav.js
+++ src/components/nav.js
@@ -8,7 +8,7 @@ export default function Nav({ navLinks }) {
   const [menuState, setMenuState] = useState(false);
 
   return (
-    <nav className="border-b border-b-gray-200">
+    <nav className="border-b border-b-gray-200 dark:border-b-gray-700">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           <NavHamburger
