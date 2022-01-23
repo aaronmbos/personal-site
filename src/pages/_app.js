4c49eac9118cb16d1ea92b4decb32c4aea553466
aaronmbos <aaronmbos@gmail.com>
2022-01-22 20:23:31 -0600

    Update theme props in app component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,7 @@ import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider forcedTheme={Component.theme || undefined} attribute="class">
+    <ThemeProvider attribute="class">
       <Component {...pageProps} />
     </ThemeProvider>
   );
