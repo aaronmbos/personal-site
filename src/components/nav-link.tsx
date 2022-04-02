adb2422ae28bcbed9b7fd237bfe9862fda462426
Aaron Bos <aaronmbos@gmail.com>
2022-04-02 12:35:02 -0500

    nav-link JS -> TSX

--- src/components/nav-link.tsx
+++ src/components/nav-link.tsx
@@ -1,6 +1,12 @@
+import { NavigationLink } from "./nav";
 import Link from "next/link";
 
-export default function NavLink({ route, text }) {
+interface Props {
+  route: string;
+  text: string;
+}
+
+export default function NavLink({ route, text }: Props) {
   return (
     <Link href={route}>
       <a className="hover:ring-2 ring-stone-400 dark:bg-stone-800 dark:hover:bg-stone-900 text-blue-500 hover:text-blue-700 dark:text-blue-300 dark:hover:text-blue-100 px-3 py-2 mt-1 rounded-md text-sm font-medium">
