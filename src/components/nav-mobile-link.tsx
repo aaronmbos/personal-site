d807b02e9a91416236e0741653f88230a8b333bf
Aaron Bos <aaronmbos@gmail.com>
2022-04-03 07:16:00 -0500

    nav-mobile-link JS -> TSX

--- src/components/nav-mobile-link.tsx
+++ src/components/nav-mobile-link.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
 
-export default function NavMobileLink({ route, text }) {
+interface Props {
+  route: string,
+  text: string
+}
+
+export default function NavMobileLink({ route, text }: Props) {
   return (
     <Link href={route}>
       <a className="dark:hover:bg-stone-900 hover:bg-blue-100 text-blue-500 dark:text-blue-300 hover:text-blue-700 dark:hover:text-blue-100 block px-3 py-2 rounded-md text-base font-medium">
