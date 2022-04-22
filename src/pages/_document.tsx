09c975eafdf917ac87000359781bb7ee440b12d7
Aaron Bos <aaronmbos@gmail.com>
2022-04-21 22:25:13 -0500

    Update social link icons to SVG to remove dependency on Font Awesome

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
@@ -9,15 +9,11 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Recursive:wght,MONO@400,1;700,1&display=swap"
         />
         <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="stylesheet"
-          href="https://use.fontawesome.com/releases/v5.13.0/css/all.css"
-        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
