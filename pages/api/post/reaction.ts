e2e188a6a29bf8ce92fb2e5d71d03ae042b51e1f
Aaron Bos <aaronmbos@gmail.com>
2022-07-18 23:02:04 -0500

    Implement client IP parsing in GET request

--- pages/api/post/reaction.ts
+++ pages/api/post/reaction.ts
@@ -2,8 +2,21 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
-    return res
-      .status(200)
-      .json(`${req.query["slug"]}: ${new Date().toUTCString()}`);
+    return res.status(200).json(`${req.query["slug"]}: ${getClientIp(req)}`);
+  }
+}
+
+function getClientIp(req: NextApiRequest): string | undefined {
+  if (req.headers.forwarded) {
+    // Originating IP will be the first entry if multiple
+    return req.headers.forwarded!.split(",")[0];
+  } else if (req.headers["x-real-ip"]) {
+    if (Array.isArray(req.headers["x-real-ip"])) {
+      return req.headers["x-real-ip"][0];
+    } else {
+      return req.headers["x-real-ip"];
+    }
+  } else {
+    return req.socket.remoteAddress;
   }
 }
