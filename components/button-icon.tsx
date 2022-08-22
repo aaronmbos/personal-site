13783359edad07d86ce2365edfa878216a300f4e
Aaron Bos <aaronmbos@gmail.com>
2022-08-21 20:08:55 -0500

    Implement copy link button

--- components/button-icon.tsx
+++ components/button-icon.tsx
@@ -1,18 +1,25 @@
 interface Props {
-  path: string;
+  path: string | string[];
   fill: string;
+  id?: string;
+  classList?: string[];
 }
 
-export default function ButtonIcon({ path, fill }: Props) {
+export default function ButtonIcon({ path, fill, id, classList }: Props) {
   return (
     <svg
+      id={id}
       xmlns="http://www.w3.org/2000/svg"
       width="17"
       height="17"
       fill={fill}
-      className="inline"
+      className={`inline ${classList?.join(" ")}`}
     >
-      <path d={path} />
+      {Array.isArray(path) ? (
+        path.map((p) => <path key={p} d={p} />)
+      ) : (
+        <path d={path} />
+      )}
     </svg>
   );
 }
