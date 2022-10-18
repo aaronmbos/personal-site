91d4b7d488f2f917ecaaad671497197d5329c69b
Aaron Bos <aaronmbos@gmail.com>
2022-10-17 19:46:27 -0500

    Use headshot from cloudinary

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   reactStrictMode: true, // https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode
   swcMinify: true,
+  images: {
+    loader: "cloudinary",
+    path: "https://res.cloudinary.com/aaron-bos/image/upload"
+  }
 };
