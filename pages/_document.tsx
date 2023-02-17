5a663daa6ab9958bdbb15b30bfff31ee7aabf8b5
Aaron Bos <aaronmbos@gmail.com>
2023-02-16 21:29:40 -0600

    WIP font optimization

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Recursive:wght,MONO@400,1;700,1&display=swap"
-        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
