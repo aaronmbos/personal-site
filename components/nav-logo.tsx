b00cf9ab1a3d4ce17b7434b283260c98102a8437
Aaron Bos <aaronmbos@gmail.com>
2023-02-17 20:01:39 -0600

    Implement font optimization

--- components/nav-logo.tsx
+++ components/nav-logo.tsx
@@ -1,17 +1,10 @@
 import Link from "next/link";
-import { Source_Code_Pro } from "@next/font/google";
-
-const sourceCodePro = Source_Code_Pro({
-  weight: ["400", "700"],
-  subsets: ["latin"],
-  variable: "--font-scp",
-});
 
 export default function NavLogo() {
   return (
     <div className="shrink-0 md:flex md:items-center">
       <Link
-        className={`dark:text-blue-300 dark:hover:text-blue-100 hover:text-blue-700 text-blue-500 cursor-pointer text-2xl ${sourceCodePro.variable} font-mono font-bold flex items-center flex-none px-2`}
+        className={`dark:text-blue-300 dark:hover:text-blue-100 hover:text-blue-700 text-blue-500 cursor-pointer text-2xl font-mono font-semibold flex items-center flex-none px-2`}
         href="/"
       >
         &lt;Aaron Bos&#47;&gt;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,28 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
+import { IBM_Plex_Mono, Inconsolata, Source_Code_Pro } from "@next/font/google";
+
+const sourceCodePro = IBM_Plex_Mono({
+  weight: ["400", "600", "700"],
+  variable: "--font-scp",
+  subsets: ["latin"],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --font-scp: ${sourceCodePro.style.fontFamily};
+          }
+        `}
+      </style>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
