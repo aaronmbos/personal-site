5f55f02108d40046a30f6b93f325d4bedd6b6bef
Aaron Bos <aaronmbos@gmail.com>
2023-03-31 06:41:56 -0500

    Testing this

--- database/db.mjs
+++ database/db.mjs
@@ -2,9 +2,9 @@ import "dotenv/config.js";
 import postgres from "postgres";
 
 const sql = postgres(process.env.DATABASE_URL, {
-  ssl: {
-    caCerts: [process.env.CERT],
-  },
+  //  ssl: {
+  //    caCerts: [process.env.CERT],
+  //  },
 });
 
 export default sql;
