29bceb29c07b173337297fb9eea2790dcc72bdaa
Aaron Bos <aaronmbos@gmail.com>
2023-06-16 19:50:10 -0500

    Fix update-algolia script

--- scripts/update-algolia.mjs
+++ scripts/update-algolia.mjs
@@ -1,40 +1,36 @@
 // Example:  node ./scripts/update-algolia.mjs '56_announce-search-feature.md'
 
-import dotenv from "dotenv";
 import algoliasearch from "algoliasearch";
-import { readdir, writeFile } from "fs/promises";
-import path from "path";
-import matter from "gray-matter";
+import { v4 as uuidv4 } from "uuid";
+import sql from "../database/db.mjs";
 import remark from "remark";
 import strip from "strip-markdown";
-import { v4 as uuidv4 } from "uuid";
 
-const fileNameArg = process.argv[2];
+async function updateAlgoliaIndex() {
+  const client = algoliasearch(
+    process.env.ALGOLIA_APP_ID,
+    process.env.ALGOLIA_ADMIN_API_KEY
+  );
+  const index = client.initIndex("aaronbos_blog_index");
 
-dotenv.config();
-const client = algoliasearch(
-  process.env.ALGOLIA_APP_ID,
-  process.env.ALGOLIA_ADMIN_API_KEY
-);
-const index = client.initIndex("aaronbos_blog_index");
+  const postsToIndex =
+    await sql`select id, title, content, slug, description, tags, (published_at at time zone 'utc') as published_at from post.post where is_indexed = false and published_at is not null order by published_at desc`;
 
-const postsDir = path.join(process.cwd(), "_posts");
-const files = await readdir(postsDir);
-const postToUpdate = files.find((n) => n === fileNameArg);
+  postsToIndex.forEach(async (post) => {
+    await index.saveObject({
+      objectID: uuidv4(),
+      title: post.title,
+      content: String(await remark().use(strip).process(post.content)),
+      slug: post.slug,
+      description: post.description,
+      metadata: post.tags,
+      date: post.published_at,
+    });
+  });
 
-if (!postToUpdate) {
-  throw Error("Can't find requested post");
+  await sql`update post.post set is_indexed = true where is_indexed = false and published_at is not null`;
+
+  console.log("Algolia index updated");
 }
 
-const matterPost = matter.read(`${postsDir}/${postToUpdate}`);
-const rawContent = await remark().use(strip).process(matterPost.content);
-
-await index.saveObject({
-  objectID: uuidv4(),
-  title: matterPost.data.title,
-  content: String(rawContent),
-  slug: matterPost.data.slug,
-  description: matterPost.data.description,
-  metadata: matterPost.data.metadata.split(","),
-  date: matterPost.data.publishedAt,
-});
+updateAlgoliaIndex().then(() => process.exit());
