04de61b061c5c8ee4a0615ad0f8fc0c2d2871f2a
Aaron Bos <aaronmbos@gmail.com>
2023-08-23 21:35:19 -0500

    Remove hover text color change from tags

--- components/tag.tsx
+++ components/tag.tsx
@@ -9,7 +9,7 @@ export default function Tag({ tag }: Props) {
     <Link
       href={`/search?q=${encodeURIComponent(`tag:${tag}`)}`}
       className="inline-block my-1 mr-1 py-1.5 px-2 rounded-md text-white dark:bg-stone-900 bg-blue-500 hover:bg-blue-700 text-sm
-      dark:hover:ring-2 dark:hover:bg-stone-900 dark:hover:text-blue-100 dark:ring-stone-400"
+      dark:hover:ring-2 dark:hover:bg-stone-900 dark:ring-stone-400"
     >
       {`#${tag}`}
     </Link>
