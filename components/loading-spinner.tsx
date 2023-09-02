833e02e601baeeb3396e5832df83396e96858fd7
Aaron Bos <aaronmbos@gmail.com>
2023-09-02 06:56:51 -0500

    Update spinner attribute

--- components/loading-spinner.tsx
+++ components/loading-spinner.tsx
@@ -11,7 +11,7 @@ function LoadingSpinner() {
         cy="12"
         r="10"
         stroke="currentColor"
-        stroke-width="4"
+        strokeWidth="4"
       ></circle>
       <path
         className="opacity-75"
