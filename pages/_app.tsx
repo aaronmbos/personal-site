25b5a28df76bb2fe33d079bb83a0a7f2698c0e81
Aaron Bos <aaronmbos@gmail.com>
2023-12-11 21:06:37 -0600

    Update to latest analytics and speed insight components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import { IBM_Plex_Mono, Inconsolata, Source_Code_Pro } from "@next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const ibmPlexMono = IBM_Plex_Mono({
   weight: ["400", "600", "700"],
@@ -21,6 +23,8 @@ function MyApp({ Component, pageProps }: AppProps) {
       </style>
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
+        <Analytics />
+        <SpeedInsights />
       </ThemeProvider>
     </>
   );
