97027c9dfd9bf6bfdf279ef744ab72383255e2bb
Aaron Bos <aaronmbos@gmail.com>
2024-01-10 05:47:55 -0600

    Remove nesting from login handler

--- lib/request-handlers/login-request.ts
+++ lib/request-handlers/login-request.ts
@@ -7,14 +7,16 @@ export async function handlePost(req: LoginRequest): Promise<User> {
     await sql`select username, password from site.user where username = ${req.username}`
   )[0];
 
-  if (user) {
-    if (await compare(req.password, user.password)) {
-      return {
-        username: req.username,
-        isLoggedIn: true,
-      };
-    }
+  if (!user) {
+    throw Error(`No user found with username ${req.username}.`);
+  }
+
+  if (!(await compare(req.password, user.password))) {
     throw Error("Invalid password.");
   }
-  throw Error(`No user found with username ${req.username}.`);
+
+  return {
+    username: req.username,
+    isLoggedIn: true,
+  };
 }
