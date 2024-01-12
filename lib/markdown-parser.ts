37261edb5a66c67bbd3018a2acc7b76681bd3c49
Aaron Bos <aaronmbos@gmail.com>
2024-01-11 19:02:55 -0600

    Package upgrades

--- lib/markdown-parser.ts
+++ lib/markdown-parser.ts
@@ -1,7 +1,17 @@
-import remark from "remark";
-import html from "remark-html";
+import rehypeDocument from "rehype-document";
+import rehypeFormat from "rehype-format";
+import rehypeStringify from "rehype-stringify";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import { unified } from "unified";
 
 export async function parseMarkdownToHtml(markdown: string) {
-  const processedMarkdown = await remark().use(html).process(markdown);
+  const processedMarkdown = await unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeDocument)
+    .use(rehypeFormat)
+    .use(rehypeStringify)
+    .process(markdown);
   return processedMarkdown.toString();
 }
