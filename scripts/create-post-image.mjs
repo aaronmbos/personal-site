28811e817d83252ceb51d7324bcadcf4939b7d72
Aaron Bos <aaronmbos@gmail.com>
2024-11-27 05:29:26 -0600

    Clean up the script a bit

--- scripts/create-post-image.mjs
+++ scripts/create-post-image.mjs
@@ -8,68 +8,79 @@ import { v2 as cloudinary } from "cloudinary";
 // The post's slug should be passed to the script
 const postSlug = process.argv[2];
 
-if (!postSlug) {
-  console.log("You need to provide a post slug.");
-}
+const post = await getPost(postSlug);
 
-const post = (
-  await sql`select id, title, slug from post.post where slug=${postSlug}`
-)[0];
+createPostImage(post);
 
-if (!post) {
-  console.log(`No post found with the provided slug: "${postSlug}"`);
-}
-
-// Spawn the child process
-const scriptPath = resolve("./scripts/create-post-image.sh");
-const child = spawn(scriptPath, ["-c", post.title, "-o", `${post.slug}.png`]);
-
-child.stdout.on("data", (data) => {
-  console.log(`${data}`);
-});
-
-// Capture standard error
-child.stderr.on("data", (data) => {
-  console.error(`An error occurred during child process. ${data}`);
-  process.exit(1);
-});
-
-// Handle process exit
-child.on("close", (code) => {
-  if (code === 0) {
-    // Make sure cloudinary returns secure URLs
-    cloudinary.config({
-      secure: true,
-    });
-
-    // Use the uploaded file's name as the asset's public ID and
-    // allow overwriting the asset with new versions
-    const options = {
-      use_filename: true,
-      unique_filename: false,
-      overwrite: true,
-    };
-
-    // Upload the image
-    cloudinary.uploader
-      .upload(`./tmp/${post.slug}.png`, options)
-      .then((result) => {
-        console.log(result);
-        updatePostImageUrl(post.id, result.secure_url)
-          .then(() => {
-            process.exit(0);
-          })
-          .catch((error) => {
-            console.error(error);
-            process.exit(1);
-          });
-      })
-      .catch((error) => {
-        console.error(error);
-        process.exit(1);
-      });
+async function getPost(slug) {
+  if (!postSlug) {
+    console.log("You need to provide a post slug.");
   }
-});
+
+  const post = (
+    await sql`select id, title, slug from post.post where slug=${postSlug}`
+  )[0];
+
+  if (!post) {
+    console.log(`No post found with the provided slug: "${postSlug}"`);
+  }
+
+  return post;
+}
+
+function createPostImage(post) {
+  const scriptPath = resolve("./scripts/create-post-image.sh");
+  const child = spawn(scriptPath, ["-c", post.title, "-o", `${post.slug}.png`]);
+
+  child.stdout.on("data", (data) => {
+    console.log(`${data}`);
+  });
+
+  child.stderr.on("data", (data) => {
+    console.error(`An error occurred during child process. ${data}`);
+    process.exit(1);
+  });
+
+  child.on("close", (code) => {
+    if (code === 0) {
+      uploadPost(post);
+    }
+  });
+}
+
+function uploadPost(post) {
+  // Make sure cloudinary returns secure URLs
+  cloudinary.config({
+    secure: true,
+  });
+
+  // Use the uploaded file's name as the asset's public ID and
+  // allow overwriting the asset with new versions
+  const options = {
+    use_filename: true,
+    unique_filename: false,
+    overwrite: true,
+  };
+
+  // Upload the image
+  cloudinary.uploader
+    .upload(`./tmp/${post.slug}.png`, options)
+    .then((result) => {
+      console.log(result);
+      updatePostImageUrl(post.id, result.secure_url)
+        .then(() => {
+          process.exit(0);
+        })
+        .catch((error) => {
+          console.error(error);
+          process.exit(1);
+        });
+    })
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
 
 async function updatePostImageUrl(id, url) {
   await sql`update post.post set image_url = ${url} where id = ${id} `;
